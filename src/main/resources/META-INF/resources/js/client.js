a5e1afeb62f027599d76adc88c5927157dc3a14f
Brian Camilo Fajardo Sánchez <briancfajardo@gmail.com>
2024-04-14 17:56:05 -0500

    se añade token

--- src/main/resources/META-INF/resources/js/client.js
+++ src/main/resources/META-INF/resources/js/client.js
@@ -1,8 +1,26 @@
 let client = (function () {
 
     let _publicFunctions = {};
-    let _stream_server = "http://localhost:8080";
-    let _tweet_server = "http://localhost:8080"
+    let _stream_server = "https://bvdo3s5ms1.execute-api.us-east-1.amazonaws.com/Beta";
+    let _tweet_server = "https://bvdo3s5ms1.execute-api.us-east-1.amazonaws.com/Beta";
+
+    let _getAccessToken = function (){
+        const urlCompleta = location.href;
+        const urlParts = urlCompleta.split('#');
+        if (urlParts.length < 2) {
+            throw new Error('Invalid URL format, missing hash fragment');
+        }
+
+        const fragmentParams = new URLSearchParams(urlParts[1]);
+
+        const idToken = fragmentParams.get('id_token');
+
+        if (!idToken) {
+            throw new Error('id_token not found in URL fragment');
+        }
+        console.log(idToken);
+        return idToken;
+    }
 
     _publicFunctions.tweet = function (author, content) {
         return $.ajax({
@@ -14,6 +32,9 @@ let client = (function () {
                 creationDate: new Date().toJSON()
             }),
             contentType: "application/json",
+            headers: {
+                'access_token': _getAccessToken()
+            }
         });
     };
 
@@ -21,6 +42,9 @@ let client = (function () {
         return $.ajax({
             url: `${_stream_server}/secured/stream`,
             type: 'GET',
+            headers: {
+                'access_token': _getAccessToken()
+            }
         });
     };
 
